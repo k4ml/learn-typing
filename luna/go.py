116e90db526a0e15fa896545f699e5257b483e48
kamal <kamal@xoxzo.com>
2023-09-25 20:29:00 +0800

    add notes

--- luna/go.py
+++ luna/go.py
@@ -5,9 +5,16 @@ class TP: pass
 
 @beartype
 def add(x: int, y: int) -> TP:
+    """
+    Change to `return 1` and beartype will throw:-
+
+    total: TP = add(1, 2)
+                ^^^^^^^^^
+  File "<@beartype(luna.go.add) at 0x7fc1feb91c60>", line 56, in add
+beartype.roar.BeartypeCallHintReturnViolation: Function luna.go.add() return 1 violates type hint <class 'luna.go.TP'> under non-default configuration BeartypeConf(claw_is_pep526=True, is_color=None, is_debug=False, is_pep484_tower=False, strategy=<BeartypeStrategy.O1: 2>, warning_cls_on_decorator_exception=<class 'beartype.roar._roarwarn.BeartypeClawDecorWarning'>), as int 1 not instance of <class "luna.go.TP">.
+    """
     return TP()
 
-@beartype
 def main():
     total: TP = add(1, 2)
 
@@ -16,5 +23,24 @@ from beartype.typing import List, TypeVar
 T = TypeVar("T")
 
 def first(container: List[T]) -> T:
-    return "a"
-    #return container[1]
+    """
+    Change to something like `return "a"` and mypy will throw:-
+
+    error: Incompatible return value type (got "str", expected "T")  [return-value]
+
+    """
+    return container[1]
+
+class Message():
+    def send(self, text: str) -> str:
+        return text
+
+class DMessage(Message):
+    """
+    mypy should throw:-
+
+    error: Signature of "send" incompatible with supertype "Message"  [override]
+    """
+
+    def send(self, text: str, num: int) -> str:
+        return text
--- run.py
+++ run.py
@@ -1,6 +1,28 @@
 
-from luna.go import main, first
-main()
+from luna.go import add, first
+add(1, 2)
+
+"""
+beartype will throw:-
+
+add(1, "a")
+  File "<@beartype(luna.go.add) at 0x7f6e3661d580>", line 40, in add
+beartype.roar.BeartypeCallHintParamViolation: Function luna.go.add() parameter y='a' violates type hint <class 'int'> under non-default configuration BeartypeConf(claw_is_pep526=True, is_color=None, is_debug=False, is_pep484_tower=False, strategy=<BeartypeStrategy.O1: 2>, warning_cls_on_decorator_exception=<class 'beartype.roar._roarwarn.BeartypeClawDecorWarning'>), as str 'a' not instance of int.
+
+mypy will throw:-
+
+error: Argument 2 to "add" has incompatible type "str"; expected "int"  [arg-type]
+"""
+#add(1, "a")
+
+
+"""
+mypy will throw:-
+
+error: Incompatible types in assignment (expression has type "TP", variable has type "str")  [assignment]
+run.py:29: error: Name "ret" already defined on line 18  [no-redef]
+"""
+ret: str = add(1, 2)
 
 from beartype.typing import List, TypeVar
 list_one: List[str] = ["a", "b", "c"]
@@ -8,3 +30,7 @@ print(first(list_one))
 
 list_two: List[int] = [1, 2, 3]
 print(first(list_two))
+
+from luna.go import DMessage
+dmesg = DMessage()
+ret: str = dmesg.send("hello", 1)
